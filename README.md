79e52b931d02b1794a6de24928a0f3734151c83c
wheelercj <chrisw289@gmail.com>
2023-02-01 17:19:41 -0800

    Change from wrapping using ctrl+c and ctrl+v to using just ctrl+'

--- README.md
+++ README.md
@@ -1,17 +1,19 @@
 # wrap
 
-Quickly wrap text.
+Quickly wrap text in **any** text editor.
 
 ![wrap demo](https://media.giphy.com/media/JBremwdj3OuFLhTsOr/giphy.gif)
-
-The default wrap width is 88 characters, but it can be easily changed.
 
 ## usage
 
 1. Download wrap.py.
 2. `pip install keyboard pyperclip`.
-3. Run the code with `py wrap.py` or `python3 wrap.py`.
-4. Copy and paste text to wrap it!
+3. Run the code with `py wrap.py` or `python3 wrap.py`. This will not wrap text yet; it just adds a temporary keyboard shortcut.
+4. Select text and press ctrl+' or cmd+' to wrap it!
 5. Press ctrl+c or cmd+c on the script when you're done wrapping.
 
 If you will use this often, you might want to [create a custom terminal command](https://wheelercj.github.io/notes/pages/20220320181252.html).
+
+The default wrap width is 88 characters. You can change the wrap width by changing the `WRAP_WIDTH` variable near the top of wrap.py.
+
+You can also change the keyboard shortcut for wrapping by changing the `WRAP_SHORTCUT` variable near the top of wrap.py.
--- wrap.py
+++ wrap.py
@@ -8,27 +8,39 @@ import keyboard
 import pyperclip
 
 
-DEFAULT_WRAP_WIDTH = 88
+WRAP_WIDTH = 88
+if platform.system() == "Darwin":  # macOS
+    WRAP_SHORTCUT = "cmd+'"
+else:
+    WRAP_SHORTCUT = "ctrl+'"
 
 
-def wrap_on_ctrl_c() -> None:
-    """Wraps text when it enters the device's clipboard."""
+def wrap_on_keyboard_shortcut() -> None:
+    """Wraps text when the user presses the keyboard shortcut."""
+    print(f"Press {WRAP_SHORTCUT} to wrap text.")
     if platform.system() == "Darwin":  # macOS
-        kb_shortcut = "cmd+c"
+        CTRL_KEY = "cmd"
     else:
-        kb_shortcut = "ctrl+c"
-    print(f"Copy (with {kb_shortcut}) and paste to wrap text.")
+        CTRL_KEY = "ctrl"
     try:
         while True:
-            keyboard.wait(kb_shortcut)
+            keyboard.wait(WRAP_SHORTCUT)
+            keyboard.send(f"{CTRL_KEY}+c")
             time.sleep(0.05)  # Wait for the clipboard to be updated.
-            pyperclip.copy(wrap_(pyperclip.paste()))
+            text = pyperclip.paste()
+            wrapped_text = wrap_(text, width=WRAP_WIDTH)
+            pyperclip.copy(wrapped_text)
+            keyboard.send(f"{CTRL_KEY}+v")
     except KeyboardInterrupt:
         print("Exiting.")
 
 
-def wrap_(text: str | None = None, width: int = DEFAULT_WRAP_WIDTH) -> str | None:
-    """Wraps text while preserving indentation.
+def wrap_(
+    text: str | None = None,
+    width: int = 88,
+    keep_indentation: bool = True,
+) -> str | None:
+    """Wraps text to a chosen width.
 
     If text is None, the text in the device's clipboard is wrapped. If text is not None,
     the clipboard is not modified and the wrapped text is returned.
@@ -42,7 +54,8 @@ def wrap_(text: str | None = None, width: int = DEFAULT_WRAP_WIDTH) -> str | Non
     text_ = text_.replace("\r\n", "\n")
     first_line = text_.split("\n")[0]
     indent_n = len(first_line) - len(first_line.lstrip())
-    indent = " " * indent_n
+    indent = " " * indent_n if keep_indentation else ""
+    width -= 0 if keep_indentation else indent_n
     texts = text_.split("\n\n")
     spaces_pattern = re.compile(r"(?<=\S)\s\s(?=\S)")
     for i, t in enumerate(texts):
@@ -62,4 +75,4 @@ def wrap_(text: str | None = None, width: int = DEFAULT_WRAP_WIDTH) -> str | Non
 
 
 if __name__ == "__main__":
-    wrap_on_ctrl_c()
+    wrap_on_keyboard_shortcut()
